9c51303967fcf46a407d684bd25ae216ce6dcb23
Luis Fernando Ushiña <guffenix@gmail.com>
2022-07-13 17:03:12 -0500

    add styles

---
+++ styles/home.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background-color: #0070f3;
+  padding: 10px;
+  border-radius: 10px;
+  color: #fff;
+  font-size: large;
+`
+
+export const Container = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: large;
+  flex-wrap: wrap;
+  text-align: center;
+`
+export const Status = styled.div`
+  margin: 20px;
+  display: flex;
+  padding: 10px;
+`
